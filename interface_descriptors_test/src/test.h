27cd0673cdfd0853fb0aa1f2e95316c7196c0b3c
Sebastian Daberdaku <yye4468@infocert.it>
2022-02-05 13:02:14 +0100

    fixed missing coordinates export for interface_descriptors_train

--- interface_descriptors_test/src/test.h
+++ interface_descriptors_test/src/test.h
@@ -17,15 +17,25 @@
 using namespace std;
 
 void export_descriptors(vector<CompactPatchDescriptor> & descriptors, size_t order, string const & outname) {
-	ofstream out_descriptors;
+	ofstream out_descriptors, out_centers, out_truth;
 	string directory = "./3DZD_test";
 	makeDirectory(directory.c_str());
-	out_descriptors.open(directory + "/" + outname + "_test_descriptors_N" + to_string(order) + ".txt", ofstream::out);
+	out_descriptors.open(directory + "/" + outname + "_test_descriptors_N" + to_string(order) + ".txt", ofstream::out | ofstream::trunc);
+	out_centers.open(directory + "/" + outname + "_patch_centers.txt", ofstream::out | ofstream::trunc);
+	out_truth.open(directory + "/" + outname + "_patch_truth.txt", ofstream::out | ofstream::trunc);
+
 	for (auto & d : descriptors) {
 		CompactPatchDescriptor::to_ostream(out_descriptors,  d, order);
 			out_descriptors << endl;
+		out_centers << d.center.x << "\t" << d.center.y << "\t" << d.center.z << "\t" << endl;
+		if (d.isInterface)
+			out_truth << "+1" << endl;
+		else
+			out_truth << "-1" << endl;
 	}
 	out_descriptors.close();
+	out_centers.close();
+	out_truth.close();
 
 }
 
